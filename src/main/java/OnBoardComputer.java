049430f6dde877c7e6a6c9b9219c7f4b5b5aeaea
Freddy Mintarja <115741236+fmintar1@users.noreply.github.com>
2022-11-25 16:17:36 -0500

    Done

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -3,9 +3,15 @@ public class OnBoardComputer implements BurnStream {
     @Override
     public int getNextBurn(DescentEvent status) {
         int burn = 0;
-        
-        System.out.println(burn); /*hack!*/
+        if(status.getVelocity() <= 1500 && status.getVelocity() > 1000 && status.getAltitude() < 14000) burn = 200;
+        if(status.getVelocity() == 1000 && status.getAltitude() < 10000) burn = 100;
+        if(status.getAltitude() > 5000 && status.getAltitude() < 6000) burn = 150;
+        if(status.getAltitude() < 5000) burn = 200;
+        if(status.getVelocity() == 150) burn = 150;
+        if(status.getVelocity() == 100 && status.getAltitude() > 100) burn = 100;
+        if(status.getVelocity() != 150 && status.getAltitude() < 100) burn = 200 - (status.getAltitude() - 1);
+        if(status.getAltitude() == 1) burn = 100 + (status.getVelocity() - 2);
+        System.out.println(burn);
         return burn;
     }
-
 }
--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,9 +1,14 @@
+import java.util.Objects;
+
 public class Vehicle {
 
-    public Vehicle(int InitialAltitude) {
-        // initialize the altitude AND previous altitude to initialAltitude
-    }
-
+    // this is initial vehicle setup
+    int Altitude= 8000;
+    int PrevAltitude= 8000;
+    int Velocity= 1000;
+    int Burn = 0;
+    int Flying = FLYING;
+    int Fuel = 12000;
     int Gravity = 100;
     /* The rate in which the spaceship descents in free fall (in ten seconds) */
 
@@ -18,16 +23,13 @@ public class Vehicle {
     public static final int SUCCESS = 0;
     public static final int FLYING = 1;
 
-    // this is initial vehicle setup
-    int Altitude= 8000;
-    int PrevAltitude= 8000;
-
-    int Velocity= 1000;
-    int Fuel = 12000;
-    int Burn = 0;
-    int Flying = FLYING;
 
     public Vehicle() {}
+    public Vehicle(int InitialAltitude) {
+        this.Altitude = InitialAltitude;
+        this.PrevAltitude = InitialAltitude;
+        // initialize the altitude AND previous altitude to initialAltitude
+    }
 
     public String checkFinalStatus() {
         String s = "";
@@ -45,16 +47,15 @@ public class Vehicle {
                 Flying = SUCCESS;
             }
         } else {
-            if (this.Altitude > 0) {
-                s = emptyfuel;
-                Flying = EMPTYFUEL;
-            } }
+            s = emptyfuel;
+            Flying = EMPTYFUEL;
+        }
         return s;
     }
 
     public int computeDeltaV() {
         // return velocity + gravity - burn amount
-        return 0;
+        return Velocity + Gravity - Burn;
     }
 
     public void adjustForBurn(int burnAmount) {
@@ -63,21 +64,36 @@ public class Vehicle {
         // set new velocity to result of computeDeltaV function.
         // subtract speed from Altitude
         // subtract burn amount fuel used from tank
+        Burn = burnAmount;
+        PrevAltitude = Altitude;
+        Velocity = computeDeltaV();
+        Altitude -= Velocity;
+        Fuel -= Burn;
     }
 
     public boolean stillFlying() {
         // return true if altitude is positive
-        return false;
+        return Altitude > 0;
     }
     public boolean outOfFuel() {
         // return true if fuel is less than or equal to zero
-        return true;
+        return Fuel <= 0;
     }
 
     public DescentEvent getStatus(int tick) {
+        int st = 0;
+        if (checkFinalStatus().equals(dead)) {
+            st = DEAD;
+        } else if (checkFinalStatus().equals(crashed)) {
+            st = CRASHED;
+        } else if (checkFinalStatus().equals(success)) {
+            st = SUCCESS;
+        } else if (checkFinalStatus().equals(emptyfuel)) {
+            st = EMPTYFUEL;
+        }
+        return new DescentEvent(tick, Velocity, Fuel, Altitude, st);
         // create a return a new DescentEvent object
         // filled in with the state of the vehicle.
-        return null;
     }
 
 }
